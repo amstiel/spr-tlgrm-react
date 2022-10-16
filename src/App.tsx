e8654453446afb4a3084b2a01a7fdf4f7b8396cd
Alexey Kunitsky <kunitsky.alexey@gmail.com>
2022-10-16 11:49:52 +1000

    Use Railway url as server & add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import {useCallback, useEffect} from 'react';
-import { products } from './mock/products';
+import { useCallback, useEffect } from 'react';
+import { useStore } from 'effector-react';
+
+import { $cart, $cartTotalAmount, $cartTotalQuantity } from './stores/cart';
 import { ProductsList } from './components/ProductsList/ProductsList';
+import { products } from './mock/products';
 
 import styles from './App.module.css';
-import { $cart, $cartTotalAmount, $cartTotalQuantity } from './stores/cart';
-import { useStore } from 'effector-react';
 
 const queryId = Telegram.WebApp.initDataUnsafe?.query_id;
 
@@ -17,15 +18,22 @@ function App() {
             products: items,
             totalPrice: cartTotalAmount,
             queryId,
-        }
-        fetch('https://spr-tlgrm-bot.herokuapp.com/order-details', {
+        };
+        Telegram.WebApp.MainButton.showProgress;
+        fetch('https://worker-production-4214.up.railway.app:8094/order-details', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(data)
+            body: JSON.stringify(data),
         })
-    }, [items])
+            .then(() => {
+                Telegram.WebApp.close();
+            })
+            .finally(() => {
+                Telegram.WebApp.MainButton.hideProgress();
+            });
+    }, [items]);
 
     useEffect(() => {
         Telegram.WebApp.ready();
@@ -35,7 +43,7 @@ function App() {
         if (cartTotalQuantity > 0) {
             Telegram.WebApp.MainButton.setParams({
                 text: `Заказать вкусностей на ${cartTotalAmount} руб.`,
-                color: '#4e9f37'
+                color: '#4e9f37',
             });
             Telegram.WebApp.MainButton.show();
         } else {
@@ -44,11 +52,11 @@ function App() {
     }, [cartTotalAmount, cartTotalQuantity]);
 
     useEffect(() => {
-        Telegram.WebApp.onEvent('mainButtonClicked', onSendData)
+        Telegram.WebApp.onEvent('mainButtonClicked', onSendData);
         return () => {
-            Telegram.WebApp.offEvent('mainButtonClicked', onSendData)
-        }
-    }, [onSendData])
+            Telegram.WebApp.offEvent('mainButtonClicked', onSendData);
+        };
+    }, [onSendData]);
 
     return (
         <div className={styles.container}>
